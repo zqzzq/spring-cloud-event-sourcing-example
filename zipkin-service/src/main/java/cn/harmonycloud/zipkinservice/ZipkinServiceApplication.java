780054bcbd9ff1069b6a9b8d2c2cfebac79b01aa
KarlCyan <404290080@qq.com>
2018-12-19 21:28:22 +0800

    update zipkin

--- zipkin-service/src/main/java/cn/harmonycloud/zipkinservice/ZipkinServiceApplication.java
+++ zipkin-service/src/main/java/cn/harmonycloud/zipkinservice/ZipkinServiceApplication.java
@@ -2,12 +2,11 @@ package cn.harmonycloud.zipkinservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin2.server.internal.EnableZipkinServer;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
 public class ZipkinServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ZipkinServiceApplication.class, args);
     }
